fee7875dfac39a091edbfa4769f530b8c40234f8
Diego Ruelas <RuelasDev@gmail.com>
2017-03-29 15:23:09 -0700

    inital commit

---
+++ mainjs.js
@@ -0,0 +1,52 @@
+/* NAVIGATION SMOOTHSCROLL */
+
+$(".main-nav  a").click(function (e) {
+  var link = $(this).attr('href');
+  
+  $('html, body').animate({
+    
+    scrollTop: $(link).offset().top
+    
+  });
+
+  e.preventDefault();
+  
+});
+
+
+/* MOBILE MENU */
+
+$('.menu').on('click', function () {
+  
+  if ($('.main-nav ul').hasClass('open')) {
+    $('.main-nav ul').removeClass('open');
+  }
+  
+  else {
+     $('.main-nav ul').addClass('open');
+  }
+  
+});
+
+/* SLIDE PLUGIN */
+
+ $(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+
+
